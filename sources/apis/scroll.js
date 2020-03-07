67d87428b101dfbfcecb155cad9940a9874a5f48
aresigner <atorresmlg@gmail.com>
2020-03-07 15:17:33 +0100

    build(api): api files

--- sources/apis/scroll.js
+++ sources/apis/scroll.js
@@ -1,8 +1,12 @@
 const scroll = (() => {
 	const scout = document.getElementById('scout');
+	const roller = document.getElementById('roller');
 	const effects = () => {
 		if (window.scrollY >= 730) {
 			scout.classList.add('animation-fadeInRight');
+			roller.classList.add('display-none');
+		} else {
+			roller.classList.remove('display-none');
 		}
 	};
 
